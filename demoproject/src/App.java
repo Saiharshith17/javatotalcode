6073fc2887c9df3e0f98930159e65b3426a9d52e
Saiharshith17 <142712608+Saiharshith17@users.noreply.github.com>
2024-11-08 17:35:24 +0530

    chcnged

--- demoproject/src/App.java
+++ demoproject/src/App.java
@@ -1,5 +1,5 @@
-
 import java.util.*;
+
 public class App {
     public static void main(String[] args) throws Exception {
         // Scanner sc=new Scanner(System.in);
@@ -7,20 +7,50 @@ public class App {
         // System.out.println(name);
         // Scanner sc=new Scanner(System.in);
         // String Name=sc.nextLine();
-        Vector<Integer> v = new Vector<>(10, 5);
-        for (int i = 0; i < 10; i++) {
-            v.add(i);
-        }
-        v.add(10, 10);      // Adding 10 at index 11
-        v.add(11);          // Adding 11 at the end
-        v.set(3, 0);        // Setting element at index 3 to 0
-        v.remove(0);        // Removing element at index 0
-        for (int i = 0; i < v.size(); i++) {
-            System.out.println(v.get(i));
-        }
         
-        
-    }
-    
+        // Vector<Integer> v = new Vector<>(10, 5);
+        // for (int i = 0; i < 10; i++) {
+        //     v.add(i);
+        // }
+        // v.add(10, 10);      // Adding 10 at index 11
+        // v.add(11);          // Adding 11 at the end
+        // v.set(3, 0);        // Setting element at index 3 to 0
+        // v.remove(0);        // Removing element at index 0
+        // for (int i = 0; i < v.size(); i++) {
+        //     System.out.println(v.get(i));
+        // }
 
+        Map<String, Integer> map = new LinkedHashMap<String, Integer>();
+        Map<String, Integer> ma = new TreeMap<String, Integer>();
+        Map<String,Integer> m=new HashMap<String,Integer>();
+        map.put("apple",1);
+        map.put("apple",1);
+        map.put("aap",2);
+        map.put("dan",3);
+        map.put("ban",4);
+         ma.put("apple",1);
+         ma.put("apple",1);
+        ma.put("aap",2);
+        ma.put("dan",3);
+        ma.put("ban",4);
+         m.put("apple",1);
+         m.put("apple",1);
+        m.put("aap",2);
+        m.put("dan",3);
+        m.put("ban",4);
+        for(Map.Entry<String,Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + entry.getValue());
+        }
+        for(Map.Entry<String,Integer> en:ma.entrySet()){
+            System.out.println(en.getKey()+en.getValue());
+        }
+        for(Map.Entry<String,Integer> e:m.entrySet()){
+            System.out.println(e.getKey()+e.getValue());
+        }
+        int value1=map.get("apple");
+        int value2=ma.get("apple");
+        m.remove("apple");
+         int value3=m.get("apple"); // here in java we are getting error when we come to this particular step after executing the other lines from the top and apple key is not present in the map m so that it throughs error but in c++ it defaults 0;
+        System.out.println(value1 + value2);
+    }
 }
