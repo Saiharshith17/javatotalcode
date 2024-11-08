af36c499f2e9e55b2c4b792b702ccbff83416e45
Saiharshith17 <142712608+Saiharshith17@users.noreply.github.com>
2024-11-08 23:04:24 +0530

    changed

--- demoproject/src/App.java
+++ demoproject/src/App.java
@@ -1,3 +1,4 @@
+
 import java.util.*;
 
 public class App {
@@ -7,7 +8,6 @@ public class App {
         // System.out.println(name);
         // Scanner sc=new Scanner(System.in);
         // String Name=sc.nextLine();
-        
         
         // Vector<Integer> v = new Vector<>(10, 5);
         // for (int i = 0; i < 10; i++) {
@@ -21,37 +21,50 @@ public class App {
         //     System.out.println(v.get(i));
         // }
 
-        Map<String, Integer> map = new LinkedHashMap<String, Integer>();
-        Map<String, Integer> ma = new TreeMap<String, Integer>();
-        Map<String,Integer> m=new HashMap<String,Integer>();
-        map.put("apple",1);
-        map.put("apple",1);
-        map.put("aap",2);
-        map.put("dan",3);
-        map.put("ban",4);
-         ma.put("apple",1);
-         ma.put("apple",1);
-        ma.put("aap",2);
-        ma.put("dan",3);
-        ma.put("ban",4);
-         m.put("apple",1);
-         m.put("apple",1);
-        m.put("aap",2);
-        m.put("dan",3);
-        m.put("ban",4);
-        for(Map.Entry<String,Integer> entry : map.entrySet()){
-            System.out.println(entry.getKey() + entry.getValue());
-        }
-        for(Map.Entry<String,Integer> en:ma.entrySet()){
-            System.out.println(en.getKey()+en.getValue());
-        }
-        for(Map.Entry<String,Integer> e:m.entrySet()){
-            System.out.println(e.getKey()+e.getValue());
-        }
-        int value1=map.get("apple");
-        int value2=ma.get("apple");
-        m.remove("apple");
-         int value3=m.get("apple"); // here in java we are getting error when we come to this particular step after executing the other lines from the top and apple key is not present in the map m so that it throughs error but in c++ it defaults 0;
-        System.out.println(value1 + value2);
+        // Map<String, Integer> map = new LinkedHashMap<String, Integer>();
+        // Map<String, Integer> ma = new TreeMap<String, Integer>();
+        // Map<String,Integer> m=new HashMap<String,Integer>();
+        // map.put("apple",1);
+        // map.put("apple",1);
+        // map.put("aap",2);
+        // map.put("dan",3);
+        // map.put("ban",4);
+        //  ma.put("apple",1);
+        //  ma.put("apple",1);
+        // ma.put("aap",2);
+        // ma.put("dan",3);
+        // ma.put("ban",4);
+        //  m.put("apple",1);
+        //  m.put("apple",1);
+        // m.put("aap",2);
+        // m.put("dan",3);
+        // m.put("ban",4);
+        // for(Map.Entry<String,Integer> entry : map.entrySet()){
+        //     System.out.println(entry.getKey() + entry.getValue());
+        // }
+        // for(Map.Entry<String,Integer> en:ma.entrySet()){
+        //     System.out.println(en.getKey()+en.getValue());
+        // }
+        // for(Map.Entry<String,Integer> e:m.entrySet()){
+        //     System.out.println(e.getKey()+e.getValue());
+        // }
+        // int value1=map.get("apple");
+        // int value2=ma.get("apple");
+        // m.remove("apple");
+        //  int value3=m.get("apple"); // here in java we are getting error when we come to this particular step after executing the other lines from the top and apple key is not present in the map m so that it throughs error but in c++ it defaults 0;
+        // System.out.println(value1 + value2);
+        
+       
+    //   Set<Integer> set=new HashSet<>();
+    //   for(int i=0;i<5;i++){
+    //       set.add(i);
+    //   }
+    //   boolean exist=set.contains(1);
+    // //   for(int i=0;i<5;i++){
+    // //       set.remove(i);
+    // //   }
+    //   int size=set.size();
+    //   System.out.println(size);
+       
     }
 }
